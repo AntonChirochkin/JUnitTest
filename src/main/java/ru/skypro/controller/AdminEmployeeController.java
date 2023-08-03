a935c94448b848744289a1d790adf4890bb5deaa
Anton <belix888@yandex.ru>
2023-08-03 19:13:07 +0500

    JUnitTest - домашняя работа.

--- src/main/java/ru/skypro/controller/AdminEmployeeController.java
+++ src/main/java/ru/skypro/controller/AdminEmployeeController.java
@@ -5,7 +5,7 @@ import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import ru.skypro.DTO.EmployeeDTO;
-import ru.skypro.EmployeeService;
+import ru.skypro.service.EmployeeService;
 
 import java.io.IOException;
 import java.io.InputStream;
